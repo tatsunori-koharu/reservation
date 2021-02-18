a516ed600ec12482e2f2e45efa24b06ab0df085b
tatsunori-koharu <tats208919ch@gmail.com>
2021-02-18 15:57:40 +0900

    order/controller編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,23 @@
 class OrdersController < ApplicationController
+
+  def new
+    @space = Space.find(params[:id])
+    order = Order.new
+  end
+
+  def create
+    @space = Space.find(params[:id])
+    order = Order.new(order_params)
+    if order.save!
+        redirect_to root_path
+    else
+      render action: :new
+  end
+
+  private
+
+  def order_params
+    permit.require(:order).merge(space_id: space.id, user_id: carrent_user.id)
+  end
+
 end
--- app/models/space.rb
+++ app/models/space.rb
@@ -1,2 +1,3 @@
 class Space < ApplicationRecord
+  has_one: oeder
 end
