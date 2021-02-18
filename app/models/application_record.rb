9f61e02ebe0eeb7cd7e38365a26d169964253902
tatsunori-koharu <tats208919ch@gmail.com>
2021-02-18 11:44:54 +0900

    devise導入

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.parmit(:sign_in_up, keys: [:name])
+  end
 end
